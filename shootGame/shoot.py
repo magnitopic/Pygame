1f7cf4303f2cd64bc125108b2cc5502e3d2d18f0
magnitopic <magnitopic@gmail.com>
2022-01-14 16:50:00 +0100

    New File structure

---
+++ shootGame/shoot.py
@@ -0,0 +1,57 @@
+import random
+import pgzrun
+import pygame
+
+puntos=0
+x1=Actor('x')
+x2=Actor('x')
+name=input("Cual es tu nombre?: ") or "player1"
+print("Elige a quien disparar.\n1 <- Alien\n2 <- apple\n3 <- luis")
+actor=input("Tu respuesta: ")
+if actor != "1" and actor != "2" and actor != "3":
+	actor="3"
+if actor=="1":
+	my_actor=Actor('alien')
+elif actor=="2":
+	my_actor=Actor('apple')
+else:
+	my_actor=Actor('luis')
+
+
+pygame.mixer.Channel(0).play(pygame.mixer.Sound('sounds/song.mp3'))
+pygame.mixer.Channel(0).set_volume(0.5)
+pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+background_image = pygame.image.load("images/background.png").convert()
+
+def draw():
+	screen.clear()
+	screen.blit(background_image, [0, 0])
+	my_actor.draw()
+	x1.draw()
+	x2.draw()
+
+def place_my_actor(): 
+	my_actor.x = random.randint(40,760)
+	my_actor.y = random.randint(40,560)
+	x1.x = 40
+	x1.y = 40
+	x2.x = 90
+	x2.y = 40
+
+def on_mouse_down(pos):
+	global puntos
+	if my_actor.collidepoint(pos):
+		puntos+=1
+		print(f"¡Buena puntería! Tienes {puntos} puntos")
+		if random.random() < .5:
+			pygame.mixer.Channel(1).play(pygame.mixer.Sound('sounds/shot_a.mp3'))
+		else:
+			pygame.mixer.Channel(1).play(pygame.mixer.Sound('sounds/shot_b.mp3'))
+		place_my_actor()
+	else:
+		print("Te has equivocado")
+		quit()
+	
+
+place_my_actor()
+pgzrun.go()
--- start.py
+++ start.py
@@ -1,13 +1,12 @@
-month=["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
-userInput=input("Introduce un mes en número: ")
-j=True
+month = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+j, userInput = True, input("Introduce un mes en número: ")
 while j:
-	try:
-		if int(userInput)-1<0:
-			raise ValueError('ERROR')
-		print(f"El mes que has elegido es {month[(int(userInput)-1)]}")
-	except:
-		print("El valor introducido es incorrecto. Vuelve a intentarlo.")
-		userInput=input("Dime un mes en número: ")
-	else:
-		j=False
+    try:
+        if int(userInput)-1 < 0:
+            raise ValueError('ERROR')
+        print(f"El mes que has elegido es {month[(int(userInput)-1)]}")
+    except:
+        print("El valor introducido es incorrecto. Vuelve a intentarlo.")
+        userInput = input("Dime un mes en número: ")
+    else:
+        j = False
